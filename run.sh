5b59af10fede48d879aeab37598bea6c420ad924
Lucas Zanella <me@lucaszanella.com>
2021-02-21 17:57:06 -0300

    maps microsd

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 xhost + && \
 sudo docker run \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
+-v $PWD/microSD:/home/project/firmware/unix/work/MicroSD \
 -e DISPLAY=unix$DISPLAY -it \
 coldcard /bin/bash \
 && xhost -
