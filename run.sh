f3ee7a1507e4eccd608589a7814de925f58d29ce
LZ <me@lucaszanella.com>
2021-11-10 21:23:53 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #sudo docker run -it coldcard /bin/bash
 xhost + && \
-sudo docker run \
+docker run \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 -v $PWD/microSD:/home/project/firmware/unix/work/MicroSD \
 -e DISPLAY=unix$DISPLAY -it \
